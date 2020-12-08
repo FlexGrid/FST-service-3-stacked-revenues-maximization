555fdcffd600784d6d84bcae464af58ae9c705b7
Dimitrios J. Vergados <vergados@ceid.upatras.gr>
2020-12-08 11:41:31 +0200

    Fix error reporting

--- swagger_server/controllers/scenario_controller.py
+++ swagger_server/controllers/scenario_controller.py
@@ -1,5 +1,7 @@
 import connexion
 import six
+import traceback
+import logging
 
 from swagger_server.models.scenario_params_inner import ScenarioParamsInner  # noqa: E501
 from swagger_server.models.scenario_result import ScenarioResult  # noqa: E501
@@ -19,15 +21,19 @@ def scenarios_post(body):  # noqa: E501
     :rtype: ScenarioResult
     """
     if connexion.request.is_json:
-        body = [ScenarioParamsInner.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
+        try:
+          body = [ScenarioParamsInner.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
 
-        result = [ScenarioResultInner.from_dict(
-            {
+          result = [ScenarioResultInner.from_dict(
+              {
                 "date": str(p.sdate),
                 "countries": [],
-            }) for p in body]
+              }) for p in body]
 
-        return result
+          return result
+        except Exception as e:
+          logging.error(traceback.format_exc())
+          return {'error_message': traceback.format_exc()}, 400
 
     return "Expecting JSON content type", 400
 
