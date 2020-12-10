54403344da23fb1f6385829a7cf48e99eb14f18f
Dimitrios J. Vergados <djvergad@gmail.com>
2020-12-10 11:29:32 +0200

    Fix indentation

--- swagger_server/controllers/scenario_controller.py
+++ swagger_server/controllers/scenario_controller.py
@@ -9,7 +9,6 @@ from swagger_server import util
 from swagger_server.models.scenario_result_inner import ScenarioResultInner  # noqa: E501
 
 
-
 def scenarios_post(body):  # noqa: E501
     """Initiates a simulation scenario
 
@@ -22,18 +21,17 @@ def scenarios_post(body):  # noqa: E501
     """
     if connexion.request.is_json:
         try:
-          body = [ScenarioParamsInner.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
+            body = [ScenarioParamsInner.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
 
-          result = [ScenarioResultInner.from_dict(
-              {
-                "date": str(p.sdate),
-                "countries": [],
-              }) for p in body]
+            result = [ScenarioResultInner.from_dict(
+                {
+                    "date": str(p.sdate),
+                    "countries": [],
+                }) for p in body]
 
-          return result
+            return result
         except Exception as e:
-          logging.error(traceback.format_exc())
-          return {'error_message': traceback.format_exc()}, 400
+            logging.error(traceback.format_exc())
+            return {'error_message': traceback.format_exc()}, 400
 
     return "Expecting JSON content type", 400
-
