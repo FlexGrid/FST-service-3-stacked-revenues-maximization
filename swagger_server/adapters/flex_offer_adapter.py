6577be597b3d705c1cb5de6e8389fedd02528d18
Dimitrios Vergados <djvergad@gmail.com>
2021-09-08 11:42:33 +0300

    Trivial case working

--- swagger_server/adapters/flex_offer_adapter.py
+++ swagger_server/adapters/flex_offer_adapter.py
@@ -5,4 +5,8 @@ def flex_offer_adapter(flex_offer_params):
     assert isinstance(flex_offer_params, FlexOfferParams)
 
     print(f"The object is {flex_offer_params}")
-    return {"ok": 1}
+    return {
+        'reserve_market_offer_up': flex_offer_params.flex_assets[0].reserve_market_offer_up,
+        'reserve_market_offer_down': flex_offer_params.flex_assets[0].reserve_market_offer_down
+    }
+        
