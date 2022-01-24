029a73c9a6676d979c71dfca5e0793e30873d5b9
Dimitrios J. Vergados <vergados@ceid.upatras.gr>
2022-01-24 09:29:53 +0200

    Fix examples, regenerate

--- regenerate.sh
+++ regenerate.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+set -euxo pipefail
+
+err() {
+  echo "**** FAIKED ****" 1>&2
+}
+trap err ERR 
+
 java -jar ../swagger-codegen-3.0.32/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -l python-flask -i ./swagger/pricing.yml
-git checkout .gitignore requirements.txt swagger_server/__main__.py swagger_server/controllers/authorization_controller.py
+
+if [ "${1:-}" == "checkout" ]
+then
+  git checkout .gitignore requirements.txt swagger_server/__main__.py swagger_server/controllers/authorization_controller.py
+fi
